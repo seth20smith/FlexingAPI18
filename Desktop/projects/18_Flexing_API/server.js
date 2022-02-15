4d6cce539ac402e06eafe0b8a664f5995e244060
Smith1 <seth20smith@gmail.com>
2022-02-14 19:06:46 -0800

    server running

--- Desktop/projects/18_Flexing_API/server.js
+++ Desktop/projects/18_Flexing_API/server.js
@@ -7,8 +7,6 @@ const PORT = process.env.PORT || 3001;
 
 
 
-
-
 // const { User } = require('./models');
 
 app.use(express.json());
@@ -17,11 +15,11 @@ app.use(express.static('public'))
 
 
 // app.use(routes);
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/virtualsdb', {
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/18_FLEXING_API', {
+//     useFindAndModify: false,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   });
   
   mongoose.set('debug', true);
   
@@ -29,28 +27,3 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/virtualsdb', {
     app.listen(PORT, () => {
       console.log(`App running on port ${PORT}!`);
     });
-  // }
-      // });
-  
-  // mongoose.set('debug', true);
-  
-// app.post('/submit', ({ body }, res) => {
-//   const user = new User(body);
-
-//   User.create(user)
-//     .then(dbUser => {
-//       res.json(dbUser);
-//     })
-//     .catch(err => {
-//       res.json(err);
-// });
-
-// app.get('/users', (req, res) => {
-//   User.find({}).then(users => {
-//     res.json(users);
-//   });
-// });
-
-
-
-// dbConnection('open'), function(){
